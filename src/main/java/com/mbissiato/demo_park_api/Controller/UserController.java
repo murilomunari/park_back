dbab85e43b227502f3530202cbdf4584e1f813a0
murilomunari <munarimurilo441@gmail.com>
2026-02-03 17:58:17 -0300

    Feat: criando algumas validações

--- src/main/java/com/mbissiato/demo_park_api/Controller/UserController.java
+++ src/main/java/com/mbissiato/demo_park_api/Controller/UserController.java
@@ -4,6 +4,7 @@ import com.mbissiato.demo_park_api.DTO.UpdatePasswordDTO;
 import com.mbissiato.demo_park_api.DTO.UserRequest;
 import com.mbissiato.demo_park_api.Entity.User;
 import com.mbissiato.demo_park_api.Service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> create (@RequestBody UserRequest data) {
+    public ResponseEntity<User> create (@Valid @RequestBody UserRequest data) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(data));
 
     }
--- src/main/java/com/mbissiato/demo_park_api/DTO/UserRequest.java
+++ src/main/java/com/mbissiato/demo_park_api/DTO/UserRequest.java
@@ -1,8 +1,20 @@
 package com.mbissiato.demo_park_api.DTO;
 
 import com.mbissiato.demo_park_api.Entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record UserRequest(String username,
-                          String password,
-                          Role role) {
-}
+
+public record UserRequest(
+        @NotBlank(message = "Username não pode estar vazio")
+        @Email(message = "Email fora do padrão")
+        String username,
+
+        @NotBlank(message = "Password não pode estar vazio")
+        @Size(min = 6, max = 9, message = "Password deve ter entre 6 e 9 caracteres")
+        String password,
+
+        Role role
+) {}
+
